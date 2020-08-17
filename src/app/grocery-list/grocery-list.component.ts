41c788d7897378919bdb57f23e62e757378a64de
MattDev97 <mattpiano52@gmail.com>
2020-08-17 12:18:51 -0400

    Added routing on grocery list.  Added back button to detail page

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { GroceryItem } from '../core/models/grocery-item';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-grocery-list',
@@ -10,8 +11,11 @@ export class GroceryListComponent implements OnInit {
 
   @Input() public groceryList : GroceryItem[];
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {}
 
+  goToItemDetail(id) {
+    this.router.navigate(['grocery-item-detail',JSON.stringify(id)]);
+  }
 }
